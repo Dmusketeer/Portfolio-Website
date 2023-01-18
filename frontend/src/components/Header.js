8fec181f027b799c5aec1be7673636d84bdbb47c
Dmuskeeter <tdheeraj9a@gmail.com>
2023-01-18 19:56:35 +0530

    Header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
 
   return (
     <>
-      <header className="flex justify-between px-5 py-2 bg-primary text-white fixed w-full z-10">
+      <header className="flex justify-between px-5 py-2 bg-primary text-[#fcd2ca] fixed w-full z-10">
         <a href="/" className="logo text-2xl font-bold text-accent">
           Dheeraj
         </a>
