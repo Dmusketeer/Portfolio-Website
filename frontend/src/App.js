2e80a6706d4db90cddd441b61b033b9a2b1faebe
Dmuskeeter <tdheeraj9a@gmail.com>
2023-01-18 19:09:20 +0530

    app component

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Blogs from "./components/Blogs";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <Hero />
+      <About />
+      <Projects />
+      <Blogs />
+      <Contact />
+      <Footer /> */}
+    </>
+  );
+}
+
+export default App;
