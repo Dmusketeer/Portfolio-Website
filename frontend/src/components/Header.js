7e9bb49e95d07be50a5ee2a18f6891a120078093
Dmuskeeter <tdheeraj9a@gmail.com>
2023-01-18 19:10:53 +0530

    Header component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { MdViewList } from "react-icons/md";
+import { CgClose } from "react-icons/cg";
+function Header() {
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () => setToggle(!toggle);
+
+  return (
+    <>
+      <header className="flex justify-between px-5 py-2 bg-primary text-white fixed w-full z-10">
+        <a href="/" className="logo text-2xl font-bold text-accent">
+          Dheeraj
+        </a>
+
+        {/* Nav for Desktop */}
+        <nav className="hidden md:block">
+          <ul className="flex">
+            <li>
+              <a href="/#about">ABOUT</a>
+            </li>
+            <li>
+              <a href="/#projects">PROJECTS</a>
+            </li>
+            <li>
+              <a href="/#blog">BLOGS</a>
+            </li>
+            <li>
+              <a href="/#contact">CONTACT</a>
+            </li>
+            <li>
+              <a href="" target="_blank" without rel="noreferrer">
+                RESUME
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Nav for Mobile */}
+        <nav
+          className={!toggle ? "mobile-nav left-[100%]" : "mobile-nav left-0"}
+        >
+          <ul className="flex flex-col">
+            <li>
+              <a href="/#about">ABOUT</a>
+            </li>
+            <li>
+              <a href="/#projects">PROJECTS</a>
+            </li>
+            <li>
+              <a href="/#blog">BLOGS</a>
+            </li>
+            <li>
+              <a href="/#contact">CONTACT</a>
+            </li>
+            <li>
+              <a href="/#resume">RESUME</a>
+            </li>
+          </ul>
+        </nav>
+
+        <button className="block md:hidden" onClick={handleToggle}>
+          {!toggle ? <MdViewList size={30} /> : <CgClose size={30} />}
+        </button>
+      </header>
+    </>
+  );
+}
+
+export default Header;
