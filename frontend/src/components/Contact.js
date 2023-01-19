0cf4b35ff4d230d55bb0234e2a18a3757afbb888
Dmuskeeter <tdheeraj9a@gmail.com>
2023-01-19 17:45:26 +0530

    Contact component

---
+++ frontend/src/components/Contact.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <section className="bg-secondery px-5 py-32" id="contact">
+      <div className="text-center md:w-[60%] mx-auto text-[#552619]">
+        <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[200px] mx-auto border-[#B2456E] pb-2">
+          Contact Me
+        </h2>
+        <p className="text-xl">
+          Open To Work As a Full-Stack Developer Role. Feel free to Contact.
+        </p>
+        <p className="py-2 text-xl">
+          <span className="font-bold">Email:</span> tdheeraj9a@gmail.com
+        </p>
+        <p className=" text-xl">
+          <span className="font-bold">Phone:</span> +91 8887867016
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
