6b0f0b5aca4b107438fdced6ff1d32d3c2dea528
Dmuskeeter <tdheeraj9a@gmail.com>
2023-01-19 17:45:44 +0530

    Footer component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="py-4 text-center bg-primary text-white">
+    <div className="py-4 text-center bg-primary text-[#FBEAE7] text-xl">
       Handcrafted by Me &copy; 2023
     </div>
   );
