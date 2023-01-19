9bf36fb44d425b2d2112d6dfe8ec62340708dc58
Dmuskeeter <tdheeraj9a@gmail.com>
2023-01-19 14:14:51 +0530

    footer component

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="py-4 text-center bg-primary text-white">
+      Handcrafted by Me &copy; 2023
+    </div>
+  );
+}
+
+export default Footer;
