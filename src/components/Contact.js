f1a1e13531797d56db31224ab53a07e927fa6d2d
Dmuskeeter <tdheeraj9a@gmail.com>
2023-12-29 20:43:47 +0530

    contactMe section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,8 +7,14 @@ function Contact() {
         <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[200px] mx-auto border-[#B2456E] pb-2">
           Contact Me
         </h2>
-        <p className="text-xl">
-          Open To Work As a Full-Stack Developer Role. Feel free to Contact.
+        <p className="text-xl text-justify">
+          I am currently open to exploring new opportunities and challenges that
+          allow me to apply my skills and contribute to innovative projects. If
+          you're looking for a Full-Stack Developer with a proven track record
+          and a passion for staying at the forefront of technology, feel free to
+          get in touch. Thank you for considering my profile, and I look forward
+          to the possibility of collaborating on exciting projects in the
+          future.
         </p>
         <p className="py-2 text-xl">
           <span className="font-bold">Email:</span> tdheeraj9a@gmail.com
