d9bbd59139a7bd030b30331b47c41a0d7c461ed5
Dmuskeeter <tdheeraj9a@gmail.com>
2023-12-30 23:00:48 +0530

    todo project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,38 +14,41 @@ function Projects() {
     },
   ];
   return (
-    <section className="bg-primary text-white px-5 py-32" id="projects">
-      <div className="container mx-auto grid md:grid-cols-2 items-center md:justify-between">
-        <div className="about-info mb-5">
-          <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[150px] border-[#B2456E] pb-2">
-            Projects
-          </h2>
-          <p className="pb-5 text-2xl">List Of Projects</p>
+    <div className="bg-primary text-white px-5 py-32">
+      <section className="bg-primary text-white px-5 py-32" id="projects">
+        <div className="container mx-auto grid md:grid-cols-2 items-center md:justify-between">
+          <div className="about-info mb-5">
+            <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[150px] border-[#B2456E] pb-2">
+              Projects
+            </h2>
+            <p className="pb-5 text-2xl">List Of Projects</p>
+          </div>
         </div>
-      </div>
-      <div className="projects container mx-auto grid md:grid-cols-3 gap-20 text-xl">
-        {Projects.map((item) => {
-          return (
-            <div>
-              <img
-                src={item.img}
-                alt={item.title}
-                className="w-[100px] h-[300px] rounded"
-              />
-              <h3 className="py-5 text-xl">{item.title}</h3>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={item.url}
-                className=" btn bg-accent rounded border-2 border-[#B2456E] text-[#fcd2ca] px-6 py-3 hover:bg-transparent"
-              >
-                Live
-              </a>
-            </div>
-          );
-        })}
-      </div>
-    </section>
+        <div className="projects container mx-auto grid md:grid-cols-3 gap-20 text-xl">
+          {Projects.map((item) => {
+            return (
+              <div>
+                <img
+                  src={item.img}
+                  alt={item.title}
+                  className="w-[100px] h-[300px] rounded"
+                />
+                <h3 className="py-5 text-xl">{item.title}</h3>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={item.url}
+                  className=" btn bg-accent rounded border-2 border-[#B2456E] text-[#fcd2ca] px-6 py-3 hover:bg-transparent"
+                >
+                  Live
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      <div className="border-b-8 border-[#FBEAE7] mx-auto lg:mx-[300px] rounded-md" />
+    </div>
   );
 }
 
